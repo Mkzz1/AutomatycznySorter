dbde7689aa18a75d54c97d522768d6d4791ccf49
Mkzz <mmikozowy@gmail.com>
2022-05-26 17:39:01 +0200

    bigger

--- AutomatycznySorter/Form1.cs
+++ AutomatycznySorter/Form1.cs
@@ -23,13 +23,13 @@ namespace AutomatycznySorter
             var periodTimeSpan = TimeSpan.FromMinutes(1);
             
             //repeat below code every 5 minutes
-            if (File.Exists(@"C:\Users\mikoz\Desktop\doki\abc.csv"))
+            if (File.Exists(@"C:\Users\Mkzz\Desktop\doki\AutoAssigment.csv"))
             {
                 var timer = new System.Threading.Timer((e) =>
                 {
                     //save every column of spreadsheet into separate text files in same folder
                     Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                    string filePath1 = @"C:\Users\mikoz\Desktop\doki\abc.csv";
+                    string filePath1 = @"C:\Users\Mkzz\Desktop\doki\AutoAssigment.csv";
                     using (var stream = File.Open(filePath1, FileMode.Open, FileAccess.Read))
                     {
                         using (var reader = ExcelReaderFactory.CreateCsvReader(stream))
@@ -37,7 +37,7 @@ namespace AutomatycznySorter
                             //every column of spreadsheet is saved into separate text file. Like column 1 is file1, column 2 is file2 etc.
                             while (reader.Read())
                             {
-                                string filePath2 = @"C:\Users\mikoz\Desktop\doki\" + reader.GetValue(0) + ".txt";
+                                string filePath2 = @"C:\Users\Mkzz\Desktop\doki\" + reader.GetValue(0) + ".txt";
                                 using (StreamWriter sw = new StreamWriter(filePath2))
                                 {
                                     for (int i = 0; i < reader.FieldCount; i++)
@@ -48,9 +48,61 @@ namespace AutomatycznySorter
                             }
                         }
                     }
-                    //Search for folder location in database based on user login. The user's login is the first line of each .txt file in the folder. Move all files that begin their name with the string of characters below the lines of the text file to the user's folder. Do for every txt file in folder.
-
-
+                    //remove first line of every .txt file in folder. Move all other lines to first line.
+                    string[] filePaths = Directory.GetFiles(@"C:\Users\Mkzz\Desktop\doki\", "*.txt");
+                    foreach (string filePath in filePaths)
+                    {
+                        string[] lines = File.ReadAllLines(filePath);
+                        if (lines.Length > 1)
+                        {
+                            File.WriteAllLines(filePath, lines.Skip(1));
+                        }
+                    }
+                    //move files
+                    string[] files = Directory.GetFiles(@"C:\Users\Mkzz\Desktop\doki\");
+                    string[] txtFiles = Directory.GetFiles(@"C:\Users\Mkzz\Desktop\doki\", "*.txt");
+                    foreach (string txtFile in txtFiles)
+                    {
+                        string txtFileName = Path.GetFileName(txtFile);
+                        string txtFileNameWithoutExtension = Path.GetFileNameWithoutExtension(txtFile);
+                        string[] txtFileContent = File.ReadAllLines(txtFile);
+                        //find FolderLocation in database.mdf file
+                        string connectionString = @"Data Source=C:\Users\Mkzz\Desktop\doki\database.mdf;Version=3;";
+                        string query = "SELECT FolderLocation FROM Users WHERE Name = '" + txtFileNameWithoutExtension + "'";
+                        //get query result as string
+                        string folderLocation = "";
+                        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                        {
+                            connection.Open();
+                            using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                            {
+                                using (SQLiteDataReader reader = command.ExecuteReader())
+                                {
+                                    while (reader.Read())
+                                    {
+                                        folderLocation = reader.GetString(0);
+                                    }
+                                }
+                            }
+                            string[] filesInFolder = Directory.GetFiles(@"C:\Users\Mkzz\Desktop\doki");
+                            foreach (string file in filesInFolder)
+                            {
+                                string fileName = Path.GetFileName(file);
+                                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+                                string[] fileContent = File.ReadAllLines(file);
+                                foreach (string line in fileContent)
+                                {
+                                    foreach (string txtLine in txtFileContent)
+                                    {
+                                        if (fileName.Contains(txtLine))
+                                        {
+                                            File.Move(file, Path.Combine(folderLocation, fileName));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }, null, startTimeSpan, periodTimeSpan);
             }
         }
