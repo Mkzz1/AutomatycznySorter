1baabfdb992d029e79cdc19f692938e1f6f4b046
mikoz <mikoz@DESKTOP-K0SFQ48>
2022-05-26 08:30:15 +0200

    smol

--- AutomatycznySorter/Form1.cs
+++ AutomatycznySorter/Form1.cs
@@ -1,4 +1,5 @@
 using ExcelDataReader;
+using System.Data.SQLite;
 using System.Text;
 
 namespace AutomatycznySorter
@@ -20,19 +21,15 @@ namespace AutomatycznySorter
         {
             var startTimeSpan = TimeSpan.Zero;
             var periodTimeSpan = TimeSpan.FromMinutes(1);
-            //if file doesnt exist, do nothing and wait for file to be created
-            if (!File.Exists(@"C:\Users\Mkzz\Desktop\doki\AutoAssigment.csv"))
-            {
-                Thread.Sleep(1000);
-            }
+            
             //repeat below code every 5 minutes
-            if (File.Exists(@"C:\Users\Mkzz\Desktop\doki\AutoAssigment.csv"))
+            if (File.Exists(@"C:\Users\mikoz\Desktop\doki\abc.csv"))
             {
                 var timer = new System.Threading.Timer((e) =>
                 {
                     //save every column of spreadsheet into separate text files in same folder
                     Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                    string filePath1 = @"C:\Users\Mkzz\Desktop\doki\AutoAssigment.csv";
+                    string filePath1 = @"C:\Users\mikoz\Desktop\doki\abc.csv";
                     using (var stream = File.Open(filePath1, FileMode.Open, FileAccess.Read))
                     {
                         using (var reader = ExcelReaderFactory.CreateCsvReader(stream))
@@ -40,7 +37,7 @@ namespace AutomatycznySorter
                             //every column of spreadsheet is saved into separate text file. Like column 1 is file1, column 2 is file2 etc.
                             while (reader.Read())
                             {
-                                string filePath2 = @"C:\Users\Mkzz\Desktop\doki\AutoAssigment" + reader.GetValue(0) + ".txt";
+                                string filePath2 = @"C:\Users\mikoz\Desktop\doki\" + reader.GetValue(0) + ".txt";
                                 using (StreamWriter sw = new StreamWriter(filePath2))
                                 {
                                     for (int i = 0; i < reader.FieldCount; i++)
@@ -51,6 +48,9 @@ namespace AutomatycznySorter
                             }
                         }
                     }
+                    //Search for folder location in database based on user login. The user's login is the first line of each .txt file in the folder. Move all files that begin their name with the string of characters below the lines of the text file to the user's folder. Do for every txt file in folder.
+
+
                 }, null, startTimeSpan, periodTimeSpan);
             }
         }
