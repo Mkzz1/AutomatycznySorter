1c58ac577e9e8f17ff05fe3b0469bca01d341c49
Mkzz <mmikozowy@gmail.com>
2022-05-28 16:54:51 +0200

    presentation version

--- AutomatycznySorter/Form1.Designer.cs
+++ AutomatycznySorter/Form1.Designer.cs
@@ -28,166 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // fileSystemWatcher1
             // 
-            this.button1.Location = new System.Drawing.Point(349, 153);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "START";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(51, 38);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(188, 23);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(289, 38);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(188, 23);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(518, 38);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(188, 23);
-            this.textBox3.TabIndex = 3;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(518, 99);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(188, 23);
-            this.textBox4.TabIndex = 6;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(289, 99);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(188, 23);
-            this.textBox5.TabIndex = 5;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(51, 99);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(188, 23);
-            this.textBox6.TabIndex = 4;
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.Filter = "*.csv";
+            this.fileSystemWatcher1.Path = "C:\\Users\\Mkzz\\Desktop\\doki";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(132, 20);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 15);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "SZZ";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(367, 20);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 15);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "WAW";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(598, 20);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 15);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "POZ";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(132, 81);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 15);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "KTW";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(367, 81);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 15);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "GDN";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(596, 81);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(34, 15);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "WRO";
+            this.label1.Size = new System.Drawing.Size(586, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URUCHOMIONO SORTOWANIE DOKUMENTÓW CCCD";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 291);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(624, 291);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SORTOWANIE DOKUMENTÓW CCCD";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private Button button1;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
-        private TextBox textBox6;
+        private FileSystemWatcher fileSystemWatcher1;
         private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private Label label6;
     }
 }
--- AutomatycznySorter/Form1.cs
+++ AutomatycznySorter/Form1.cs
@@ -11,133 +11,95 @@ namespace AutomatycznySorter
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
         {
-            //start SZZSort method
-            SZZSort();
-        }
-
-        private void SZZSort()
-        {
-            var startTimeSpan = TimeSpan.Zero;
-            var periodTimeSpan = TimeSpan.FromMinutes(1);
-
-            var timer = new System.Threading.Timer((e) =>
+            //save every column of spreadsheet into separate text files in same folder
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            string filePath1 = @"C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\AutoAssigment.csv";
+            using (var stream = File.Open(filePath1, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateCsvReader(stream))
                 {
-                    //save every column of spreadsheet into separate text files in same folder
-                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                    string filePath1 = @"C:\Users\Mkzz\Desktop\doki\AutoAssigment.csv";
-                    using (var stream = File.Open(filePath1, FileMode.Open, FileAccess.Read))
+                    //every column of spreadsheet is saved into separate text file. Like column 1 is file1, column 2 is file2 etc.
+                    while (reader.Read())
                     {
-                        using (var reader = ExcelReaderFactory.CreateCsvReader(stream))
+                        string filePath2 = @"C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\" + reader.GetValue(0) + ".txt";
+                        using (StreamWriter sw = new StreamWriter(filePath2))
                         {
-                            //every column of spreadsheet is saved into separate text file. Like column 1 is file1, column 2 is file2 etc.
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                sw.WriteLine(reader.GetValue(i));
+                            }
+                        }
+                    }
+                }
+            }
+            //remove first line of every .txt file in folder. Move all other lines to first line.
+            string[] filePaths = Directory.GetFiles(@"C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\", "*.txt");
+            foreach (string filePath in filePaths)
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                if (lines.Length > 1)
+                {
+                    File.WriteAllLines(filePath, lines.Skip(1));
+                }
+            }
+            //get file names from folder WITHOUT path
+            string[] userNames = Directory.GetFiles(@"C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\", "*.txt");
+            //remove path from fileNames
+            for (int i = 0; i < userNames.Length; i++)
+            {
+                userNames[i] = Path.GetFileNameWithoutExtension(userNames[i]);
+            }
+            List<string> folderLocaions = new List<string>();
+            //add to list folderLocations FolderLocation from database.mdf of specific user from list userNames
+            using (SQLiteConnection conn = new SQLiteConnection(@"Data Source=C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\database.mdf;Version=3;"))
+            {
+                conn.Open();
+                foreach (string user in userNames)
+                {
+                    string sql = "SELECT FolderLocation FROM Users WHERE Name = '" + user + "'";
+                    using (SQLiteCommand command = new SQLiteCommand(sql, conn))
+                    {
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
                             while (reader.Read())
                             {
-                                string filePath2 = @"C:\Users\Mkzz\Desktop\doki\" + reader.GetValue(0) + ".txt";
-                                using (StreamWriter sw = new StreamWriter(filePath2))
-                                {
-                                    for (int i = 0; i < reader.FieldCount; i++)
-                                    {
-                                        sw.WriteLine(reader.GetValue(i));
-                                    }
-                                }
+                                folderLocaions.Add(reader.GetString(0));
                             }
                         }
                     }
-                    //remove first line of every .txt file in folder. Move all other lines to first line.
-                    string[] filePaths = Directory.GetFiles(@"C:\Users\Mkzz\Desktop\doki\", "*.txt");
-                    foreach (string filePath in filePaths)
+                }
+            }
+            //Move all files that contain charachters from .txt file, new line is a separator. .txt file name is user Name, move to user folder.
+            for (int i = 0; i < userNames.Length; i++)
+            {
+                string[] lines = File.ReadAllLines(@"C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\" + userNames[i] + ".txt");
+                foreach (string line in lines)
+                {
+                    string[] words = line.Split('\n');
+                    foreach (string word in words)
                     {
-                        string[] lines = File.ReadAllLines(filePath);
-                        if (lines.Length > 1)
+                        if (word.Length > 0)
                         {
-                            File.WriteAllLines(filePath, lines.Skip(1));
-                        }
-                    }
-                    //get file names from folder WITHOUT path
-                    string[] userNames = Directory.GetFiles(@"C:\Users\Mkzz\Desktop\doki\", "*.txt");
-                    //remove path from fileNames
-                    for (int i = 0; i < userNames.Length; i++)
-                    {
-                        userNames[i] = Path.GetFileNameWithoutExtension(userNames[i]);
-                    }
-                    List<string> folderLocaions = new List<string>();
-                    //add to list folderLocations FolderLocation from database.mdf of specific user from list userNames
-                    using (SQLiteConnection conn = new SQLiteConnection(@"Data Source=C:\Users\Mkzz\Desktop\doki\database.mdf;Version=3;"))
-                    {
-                        conn.Open();
-                        foreach (string user in userNames)
-                        {
-                            string sql = "SELECT FolderLocation FROM Users WHERE Name = '" + user + "'";
-                            using (SQLiteCommand command = new SQLiteCommand(sql, conn))
+                            //move all files that start with word
+                            string[] files = Directory.GetFiles(@"C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\", word + "*");
+                            foreach (string file in files)
                             {
-                                using (SQLiteDataReader reader = command.ExecuteReader())
-                                {
-                                    while (reader.Read())
-                                    {
-                                        folderLocaions.Add(reader.GetString(0));
-                                    }
-                                }
+                                File.Move(file, folderLocaions[i] + "\\" + Path.GetFileName(file));
                             }
                         }
                     }
-                    //Move all files that contain charachters from .txt file, new line is a separator. .txt file name is user Name, move to user folder.
-                    for (int i = 0; i < userNames.Length; i++)
-                    {
-                        string[] lines = File.ReadAllLines(@"C:\Users\Mkzz\Desktop\doki\" + userNames[i] + ".txt");
-                        foreach (string line in lines)
-                        {
-                            string[] words = line.Split('\n');
-                            foreach (string word in words)
-                            {
-                                if (word.Length > 0)
-                                {
-                                    //move all files that start with word
-                                    string[] files = Directory.GetFiles(@"C:\Users\Mkzz\Desktop\doki\", word + "*");
-                                    foreach (string file in files)
-                                    {
-                                        File.Move(file, folderLocaions[i] + "\\" + Path.GetFileName(file));
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    //delete all .txt files in folder
-                    string[] txtFiles = Directory.GetFiles(@"C:\Users\Mkzz\Desktop\doki\", "*.txt");
-                    foreach (string file in txtFiles)
-                    {
-                        File.Delete(file);
-                    }
-                    //move AutoAssigment.csv file to folder C:\Users\mikoz\Desktop\doki\User4 named as hour minute and date of move
-                    File.Move(@"C:\Users\Mkzz\Desktop\doki\AutoAssigment.csv", @"C:\Users\Mkzz\Desktop\doki\Test6\" + "SZZ " + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".csv");
-                }, null, startTimeSpan, periodTimeSpan);
-        }
-
-
-        private void WAWSort()
-        {
-            
-        }
-
-        private void POZSort()
-        {
-
-        }
-
-        private void KTWSort()
-        {
-
-        }
-
-        private void GDNSort()
-        {
-
-        }
-
-        private void WROSort()
-        {
-
+                }
+            }
+            //delete all .txt files in folder
+            string[] txtFiles = Directory.GetFiles(@"C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\", "*.txt");
+            foreach (string file in txtFiles)
+            {
+                File.Delete(file);
+            }
+            //move AutoAssigment.csv file to folder C:\Users\mikoz\Desktop\doki\User4 named as hour minute and date of move
+            File.Move(@"C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\AutoAssigment.csv", @"C:\Users\mikujawa\Desktop\Prezentacja\OTRS2\Logi\" + "SZZ " + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".csv");
         }
     }
 }
